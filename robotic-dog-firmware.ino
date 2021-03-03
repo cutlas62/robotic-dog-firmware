b0acb0876de917efe7dbe6e5ab665591a227e3bd
Carlos <carlos.santosr@ieee.org>
2021-03-03 17:18:52 -0500

    Add more features to the serial CLI. We can now set and get the legs offsets from the terminal

--- robotic-dog-firmware.ino
+++ robotic-dog-firmware.ino
@@ -30,14 +30,16 @@ void loop() {
     int y = -70;
     for (int x = -30; x <= 60; x += 1) {
         frLeg.moveFoot(pwm, x, y);
+        flLeg.moveFoot(pwm, x, y);
         delay(8);
     }
     for (int x = 60; x >= -30; x -= 1) {
         frLeg.moveFoot(pwm, x, y);
+        flLeg.moveFoot(pwm, x, y);
         delay(8);
     }
 
-    delay(100);
+    //delay(100);
 }
 
 void checkSerialPort(void) {
@@ -45,21 +47,15 @@ void checkSerialPort(void) {
         // Read the incoming string and null-terminate it
         char inputBuf [INPUT_BUF_SIZE + 1];
         byte inNBytes = Serial.readBytes(inputBuf, INPUT_BUF_SIZE);
-        inputBuf[inNBytes] = 0;
+        inputBuf[inNBytes - 1] = 0;
 
         // Parse the incoming string
         int argc;
         char* argv [MAX_INPUT_COMMANDS];
         parseInputBuf(inputBuf, &argc, argv);
 
-        Serial.print("argc = ");
-        Serial.println(argc);
-        for (int i = 0; i < argc; i++) {
-            Serial.print(i);
-            Serial.print(" -> ");
-            Serial.println(argv[i]);
-        }
-
+        // Decode the incoming command
+        decodeInputCmd(argc, argv);
     }
 }
 
@@ -74,7 +70,7 @@ void parseInputBuf (char* inBuf, int* argc, char* argv []) {
             lastTok = i + 1;
             inBuf[i] = NULL;
 
-            if (*argc == MAX_INPUT_COMMANDS){
+            if (*argc == MAX_INPUT_COMMANDS) {
                 // Return here to avoid buffer overflow in argv
                 return;
             }
@@ -87,4 +83,74 @@ void parseInputBuf (char* inBuf, int* argc, char* argv []) {
         argv[*argc] = &inBuf[lastTok];
         (*argc)++;
     }
+
+}
+
+int decodeInputCmd (int argc, char* argv []) {
+    /*
+    for (int i = 0; i < argc; i++){
+        Serial.print("argv[");
+        Serial.print(i);
+        Serial.print("] -> ");
+        Serial.println(argv[i]);
+    }
+    */
+    if (argc < 1) {
+        return -1;
+    }
+
+    Leg *targetLeg;
+
+    if (strcmp(argv[0], "help") == 0) {
+        printHelp();
+    } else if (strcmp(argv[0], "fr") == 0) {
+        targetLeg = &frLeg;
+    } else if (strcmp(argv[0], "fl") == 0) {
+        targetLeg = &flLeg;
+    } else if (strcmp(argv[0], "rr") == 0) {
+        targetLeg = &rrLeg;
+    } else if (strcmp(argv[0], "rl") == 0) {
+        targetLeg = &rlLeg;
+    } else {
+        return -2;
+    }
+
+    if (strcmp(argv[1], "get") == 0) {
+        // Check that there are at least 1 more argument
+        if (argc < 3) {
+            return -3;
+        }
+        if (strcmp(argv[2], "kneeOffset") == 0) {
+            Serial.println(targetLeg->getKneeOffset());
+        } else if (strcmp(argv[2], "hipOffset") == 0) {
+            Serial.println(targetLeg->getHipOffset());
+        } else {
+            return -5;
+        }
+
+    } else if (strcmp(argv[1], "set") == 0) {
+        // Check that there are at least 2 more arguments
+        if (argc < 4) {
+            return -4;
+        }
+
+        // Get the target value
+        int targetVal = atoi(argv[3]);
+
+        if (strcmp(argv[2], "kneeOffset") == 0) {
+            targetLeg->setKneeOffset(targetVal);
+            Serial.println("Ok");
+        } else if (strcmp(argv[2], "hipOffset") == 0) {
+            targetLeg->setHipOffset(targetVal);
+            Serial.println("Ok");
+        } else {
+            return -6;
+        }
+    } else {
+        return -7;
+    }
+}
+
+void printHelp (void) {
+    Serial.println("WIP");
 }
